eece5d7d45fd8ce35a0cdf9e48e8b5b1ecb4d80e
Yury Minakov <minakov_uri@mail.ru>
2019-10-08 09:24:23 +0300

    вынес все типы в файл types.h

--- MealyMooreMachines/MealyMooreMachines/MooreMachine.h
+++ MealyMooreMachines/MealyMooreMachines/MooreMachine.h
@@ -1,15 +1,8 @@
 #pragma once
 #include <iostream>
-#include <map>
 #include "types.h"
 
 using namespace std;
-
-struct MooreMachineData
-{
-	map<InputSignal, map<State, State>> graph;
-	map<State, OutputSignal> outputSignals;
-};
 
 MooreMachineData ReadMooreMachineData(istream& stream, int inputAlphabetCharsCount, int statesCount);
 void PrintMooreMachineData(const MooreMachineData& mooreMachineData, ostream& stream);
--- MealyMooreMachines/MealyMooreMachines/types.h
+++ MealyMooreMachines/MealyMooreMachines/types.h
@@ -1,8 +1,23 @@
 #pragma once
 #include <string>
+#include <map>
 
 using namespace std;
 
 using State = string;
 using InputSignal = string;
 using OutputSignal = string;
+
+struct MealyTransition
+{
+	State state;
+	OutputSignal outputSignal;
+};
+
+using MealyMachineData = map<InputSignal, map<State, MealyTransition>>;
+
+struct MooreMachineData
+{
+	map<InputSignal, map<State, State>> graph;
+	map<State, OutputSignal> outputSignals;
+};
